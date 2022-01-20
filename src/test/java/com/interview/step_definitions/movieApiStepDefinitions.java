9e1e39229567e36d8058f3db7503ae7938e55aa7
borusik789 <borusik789@gmail.com>
2022-01-20 20:57:10 +0300

    mvn test

--- src/test/java/com/interview/step_definitions/movieApiStepDefinitions.java
+++ src/test/java/com/interview/step_definitions/movieApiStepDefinitions.java
@@ -3,27 +3,19 @@ package com.interview.step_definitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
-public class movieAPI_steps {
-
-
+public class movieApiStepDefinitions {
 
     @Given("The Open Movie Database")
-    public void the_open_movie_database() {
+    public void theOpenMovieDatabase() {
         System.out.println("We are on a The Open Movie Database");
     }
 
-
-    @DisplayName("Test Search Movie or OpenMovieDB Test")
-    @Test
     @Then("user can verify API calls for different movies")
-    public void user_can_verify_api_calls_for_different_movies() {
-
+    public void userCanVerifyAPICallsForDifferentMovies() {
         given()
                 .queryParam("apiKey", "5b5d0fe8")
                 .queryParam("t", "The Orville").
@@ -34,4 +26,6 @@ public class movieAPI_steps {
                 .contentType(ContentType.JSON)
                 .body("Ratings[0].Source", is("Internet Movie Database"));
     }
+
+
 }
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -7,15 +7,15 @@ Feature: As user I want to be able to login
   @smoke
   Scenario: Login with valid credentials
     When user logs in with valid credentials
-    Then user should see "My account - My Store" page
+    Then user should see "My account - My Stor" page
 
-  @negative_login @smoke @UI
+  @negative_login
   Scenario Outline: Invalid login with <email> and <password>
     When user logs in with "<email>" username and "<password>" password
     Then user verifies that "<message>" message is displayed
     Examples:
       | email            | password | message                    |
-      | bob789@gmail.com | qwer     | Invalid password.          |
+      | bob789@gmail.com | qwer     | Invalid password           |
       | bob789@gmail.com |          | Password is required.      |
       | wrong@gmail.com  | b1234567 | Authentication failed.     |
       |                  | b1234567 | An email address required. |
